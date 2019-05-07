90142e9cb46210ccb8b9b96fd410b0cd44627abf
Ondrej Jamriska <ondrej.jamriska@gmail.com>
2019-05-07 13:02:54 +0200

    use asmjs instead of wasm with emscripten (for compatibility)

--- build-emscripten.bat
+++ build-emscripten.bat
@@ -1,1 +1,1 @@
-em++ main.cpp styleblit/styleblit.cpp -I"." -I"styleblit" -s TOTAL_MEMORY=33554432 -s USE_GLFW=3 -std=c++0x -DNDEBUG -O3 --preload-file styleblit --preload-file data -o styleblit.html
+em++ main.cpp styleblit/styleblit.cpp -I"." -I"styleblit" -s WASM=0 -s TOTAL_MEMORY=33554432 -s USE_GLFW=3 -std=c++0x -DNDEBUG -O3 --preload-file styleblit --preload-file data -o styleblit.html
